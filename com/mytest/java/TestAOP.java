03b70541a8aab411f46526c033f8ea7788e4b52f
NicklousYong <2691302800@qq.com>
2021-08-09 15:40:14 +0800

    First Version

---
+++ com/mytest/java/TestAOP.java
@@ -0,0 +1,17 @@
+package com.mytest.java;/*
+ * @AUTHOR   LiXiang
+ * @Classname TestAOP
+ *
+ * @Date 2021/8/9 обнГ3:27
+ *
+
+ * @userName
+ */
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+public class TestAOP {
+
+
+}
